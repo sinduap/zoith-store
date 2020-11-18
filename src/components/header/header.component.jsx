0aab4d7954cb990cc899f8f014eb22ee2f3e3ca9
Sindu Andita <sinduandita@gmail.com>
2020-11-19 04:07:13 +0700

    Add sign-in-and-sign-up-page, SignIn, and FormInput Component + styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/zoith.svg";
 import "./header.styles.scss";
 
 const Header = () => (
